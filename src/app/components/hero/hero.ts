7929d05f10b3c2871a74f0651ce343b52a851fc5
Lasdepic <clementjordan34@gmail.com>
2026-05-14 10:55:15 +0200

    Information et A propos de moi

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Information } from '../information/information.js';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [Information],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Hero {}
+export class Hero {
+  readonly city = 'Montpellier';
+  readonly phone = '0613947403';
+  readonly mail = 'clementjordan34@gmail.com';
+  readonly githubUrl = 'https://github.com/Lasdepic';
+  readonly linkedinUrl = 'https://www.linkedin.com/in/jordan-bellin-95ab7022b/';
+}
