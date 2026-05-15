4facb9e762349505eff16f6efab8da1b3e3483da
Lasdepic <clementjordan34@gmail.com>
2026-05-15 10:51:53 +0200

    ajout page github

--- src/app/core/routes/app.routes.ts
+++ src/app/core/routes/app.routes.ts
@@ -1,24 +1,29 @@
 import { Routes } from '@angular/router';
 
 import { FreshtrackPage } from '../../pages/freshtrack/freshtrack';
+import { GithubPage } from '../../pages/github/github';
 import { Home } from '../../pages/home/home';
 import { NabuPage } from '../../pages/nabu/nabu';
 
 export const routes: Routes = [
-	{
-		path: '',
-		component: Home,
-	},
-	{
-		path: 'projets/freshtrack',
-		component: FreshtrackPage,
-	},
-	{
-		path: 'projets/nabu',
-		component: NabuPage,
-	},
-	{
-		path: '**',
-		redirectTo: '',
-	},
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'projets/freshtrack',
+    component: FreshtrackPage,
+  },
+  {
+    path: 'projets/nabu',
+    component: NabuPage,
+  },
+  {
+    path: 'github',
+    component: GithubPage,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
